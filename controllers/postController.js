be442f2d6f2b83d755d21041b52d7cea52fc0d6f
ilariamotta <ilaria.motta93@gmail.com>
2025-12-18 16:46:26 +0100

    feat: logiche crud completate

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,28 +1,47 @@
 import posts from "../data.js"
 
 function index(req, res) {
+    const tags = req.query.tags;
+
+    let filteredVideogames = posts;
+    if (tags !== undefined) {
+        filteredVideogames = posts.filter((videogame) =>
+            videogame.tags.includes(tags)
+        )
+    }
+
     const listaVideogiochi = {
-        totale: posts.length,
-        risultati: posts,
+        totale: filteredVideogames.length,
+        risultati: filteredVideogames,
 
     }
     res.json(listaVideogiochi)
 }
 
 function show(req, res) {
-const id = parseInt(req.params.id);
+    const id = parseInt(req.params.id);
     const videogioco = posts.find(game => game.id === id)
-    res.json(videogioco)
+
+    if (videogioco !== undefined) {
+        res.json(videogioco)
+    } else {
+        res.status(404)
+        res.json({
+            error: "Not found",
+            message: "Videogioco non in elenco"
+        })
+    }
+
 }
 
 
-function store(req, res){
+function store(req, res) {
     console.log("creo nuovo gioco");
     res.send("Creo nuovo videogioco")
 }
 
 function update(req, res) {
-console.log("modifico videogioco")
+    console.log("modifico videogioco")
     const id = req.params.id;
     res.send("Aggiorno videogioco " + id)
 }
@@ -34,9 +53,18 @@ function modify(req, res) {
 }
 
 function destroy(req, res) {
-    console.log("elimino videogioco")
-    const id = req.params.id;
-    res.send("Elimino videogioco " + id)
+    const id = parseInt(req.params.id);
+    const index = posts.findIndex((gioco) => gioco.id === id)
+    if (index === -1) {
+        res.status(404)
+        res.json({
+            error: "Not found",
+            message: "Videogioco non trovato"
+        })
+    } else {
+        posts.splice(index, 1)
+    }
+    res.sendStatus(204)
 }
 
 const controller = {
